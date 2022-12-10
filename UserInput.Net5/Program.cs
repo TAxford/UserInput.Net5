701b8270936a2cf32a04769aea530e99d4b261df
Timothy Axford <t.axford2@yahoo.com>
2022-12-09 21:40:14 -0500

    Added a code for a user to input a number, then a second number and it put the numbers together.

--- UserInput.Net5/Program.cs
+++ UserInput.Net5/Program.cs
@@ -4,12 +4,25 @@ namespace UserInput.Net5
 {
     internal class Program
     {
-        static void Main(string[] args)
+        /*static void Main(string[] args)
         {
-            //Promts user to write something
+            //Prompts user to write something
             string input = Console.ReadLine();
             Console.WriteLine(input);
             Console.Read();
+        }*/
+
+        static void Main(string[] args)
+        {
+            //Prompts user to write something
+            Console.WriteLine("Please enter a number");
+            string input = Console.ReadLine();
+            Console.WriteLine("Please enter a number");
+            string input2 = Console.ReadLine();
+            Console.WriteLine(input + input2);
+            Console.Read();
         }
+
+
     }
 }
