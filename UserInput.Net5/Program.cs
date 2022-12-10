99fa89b10eed31b0a77865f0730808171ba31144
Timothy Axford <t.axford2@yahoo.com>
2022-12-09 21:47:03 -0500

    Added code to take to numbers inputed by the user and add them.

--- UserInput.Net5/Program.cs
+++ UserInput.Net5/Program.cs
@@ -12,7 +12,7 @@ namespace UserInput.Net5
             Console.Read();
         }*/
 
-        static void Main(string[] args)
+        /*static void Main(string[] args)
         {
             //Prompts user to write something
             Console.WriteLine("Please enter a number");
@@ -21,6 +21,29 @@ namespace UserInput.Net5
             string input2 = Console.ReadLine();
             Console.WriteLine(input + input2);
             Console.Read();
+        }*/
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Calculate());
+            Console.Read();
+        }
+
+        public static int Calculate()
+        {
+
+            Console.WriteLine("Please enter the first number");
+            string number1 = Console.ReadLine();
+            Console.WriteLine("Please enter the second number");
+            string number2 = Console.ReadLine();
+
+            int input1 = int.Parse(number1);
+            int input2 = int.Parse(number2);
+
+            int result = input1 + input2;
+
+
+            return result;
         }
 
 
